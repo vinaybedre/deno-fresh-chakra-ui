0ae4da6dcf6a53ce0f70491427694d1a2baf338f
Vinay Bedre <vinay.bedre@sawayo.de>
2023-01-26 13:51:48 +0000

    chakra: Add chakra ui

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,6 +1,5 @@
 import { useState } from "preact/hooks";
-import { Button } from "../components/Button.tsx";
-
+import { Button, ChakraProvider } from "@chakra-ui/react";
 interface CounterProps {
   start: number;
 }
@@ -10,8 +9,10 @@ export default function Counter(props: CounterProps) {
   return (
     <div>
       <p>{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <ChakraProvider>
+        <Button onClick={() => setCount(count - 1)}>-1</Button>
+        <Button onClick={() => setCount(count + 1)}>+1</Button>
+      </ChakraProvider>
     </div>
   );
 }
